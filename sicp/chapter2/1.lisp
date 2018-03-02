3670ff955db353adf86cf00e4a4469d21070c864
jimmy <aiwei18@gmail.com>
2018-03-02 18:30:42 +0800

    ds

---
+++ sicp/chapter2/1.lisp
@@ -0,0 +1,47 @@
+(define make-rat cons)
+
+(define (number x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+    (newline)
+	  (display (number x))
+	    (display "/")
+		  (display (denom x)))
+
+(define one-half (make-rat 1 2))
+
+
+(define (add-rat x y)
+    (make-rat (+ (* (number x) (denom y)) 
+				                   (* (denom x) (number y)))
+			                  (* (denom x) (denom y))))
+              
+(define (sub-rat x y)
+    (make-rat (- (* (number x) (denom y))
+				                (* (number y) (denom x)))
+			               (* (denom x) (denom y))))
+           
+(define (mul-rat x y)
+    (make-rat (* (number x) (number y))
+			              (* (denom x) (denom y))))
+          
+(define (div-rat x y)
+    (make-rat (* (number x) (denom y))
+			              (* (denom x) (number y))))
+          
+(define (equal-rat? x y)
+    (= (* (number x) (denom y))
+	        (* (number y) (denom x))))
+   
+   
+(define (make-rat x y)
+    (if (> (* x y) 0)
+	        (if (< x 0) 
+			            (cons (- x) (- y))
+						          (cons x y))
+			      (if (> x 0) 
+					          (cons (- x) (- y))
+							            (cons x y))))
+      
